7021f6c4c576866ecf6e464f9804f6e8c02e43e5
Irene <a_irene.barboteo.company@iespablopicasso.es>
2021-11-30 12:03:33 +0100

    Creación de la clase NumAleatorios

---
+++ src/NumAleatorios.java
@@ -0,0 +1,13 @@
+
+public class NumAleatorios {
+
+	public static void main(String[] args) {
+		int[] array=new int[30];
+
+        for (int i=0; i<array.length; i++){
+            array[i]=(int)(Math.random()*10+1);
+        }
+
+	}
+
+}
