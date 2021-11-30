ac7f4ac270b8276c3fb67a6b9e6fa1b19b2c1442
Irene <a_irene.barboteo.company@iespablopicasso.es>
2021-11-30 13:01:33 +0100

    Cambio el contenido del array a 20 numeros

--- src/NumAleatorios.java
+++ src/NumAleatorios.java
@@ -2,7 +2,7 @@
 public class NumAleatorios {
 
 	public static void main(String[] args) {
-		int[] array=new int[30];
+		int[] array=new int[20];
 
         for (int i=0; i<array.length; i++){
             array[i]=(int)(Math.random()*10+1);
