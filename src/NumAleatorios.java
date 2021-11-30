5f8c3e54dde0e1676d7a12534f068d84805a9220
Irene <a_irene.barboteo.company@iespablopicasso.es>
2021-11-30 12:48:06 +0100

    Añado un método para contar cuantas veces se repite cada numero

--- src/NumAleatorios.java
+++ src/NumAleatorios.java
@@ -13,6 +13,17 @@ public class NumAleatorios {
         }
         System.out.println("\n");
 
-	}
+        int numero = 1;
 
+        while(numero<=10){
+            int contador = 0;
+            for (int j=0; j<array.length; j++){
+             if(numero==array[j]){
+                 contador++;
+             }
+            }
+            System.out.println(numero + " aparece " + contador + " vez/veces");
+            numero ++;
+        }
+	}
 }
